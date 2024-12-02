230931cdb0e8d8a248195998de05ec3b2a902fe1
Vinay <vinayrathi786@gmail.com>
2024-12-02 15:01:01 +0530

    Deploy: Prepare project for Netlify deployment

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-alert("Hello")
+import React, { useEffect } from 'react';
+
+function App() {
+  // useEffect hook will run when the component is mounted (like componentDidMount)
+  useEffect(() => {
+    alert('Welcome to DevTinder FE!');
+  }, []); // Empty dependency array means it will only run once after the component mounts.
+
+  return (
+    <div className="App">
+      <h1>Hello, DevTinder!</h1>
+    </div>
+  );
+}
+
+export default App;

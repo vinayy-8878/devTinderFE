b24768ec2522db2383bccba3416f36140f689100
Vinay <vinayrathi786@gmail.com>
2024-12-13 12:30:18 +0530

    build

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import React, { useEffect } from 'react';
 function App() {
   // useEffect hook will run when the component is mounted (like componentDidMount)
   useEffect(() => {
-    alert('Welcome to DevTinder FE!');
+    
   }, []); // Empty dependency array means it will only run once after the component mounts.
 
   return (
     <div className="App">
-      <h1>Hello, DevTinder!</h1>
+     
     </div>
   );
 }
